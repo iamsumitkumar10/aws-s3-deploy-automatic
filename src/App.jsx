ab0e634f14ba6a8dad0e69825605d2296fd516ec
Sumit Kumar <sumitku3500@gmail.com>
2025-05-04 12:36:37 +0530

    modifying app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-        <h2>automatic deploy in aws s3 and send a mail notification to successfully uploaded</h2>
+        <h2>automatic upload and deploy in aws s3 and send a mail notification to successfully deploy</h2>
     </>
   )
 }
