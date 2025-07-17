42dc2667ba503af5a43248f8538c190ec96d2bb5
sumit kumar <sumitku3500@gmail.com>
2025-07-17 21:04:12 +0530

    add project workflow

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 
-
-automatic git scm pool , install dependencies, build package and deploy in aws s3 bucket static web hosting.
-
+🚀 Project Deployment Workflow
+Code changes are made and pushed to the Git repository.
+Jenkins automatically fetches the latest code from Git.
+Using Jenkins scripts, the project is compiled and a production build is generated.
+The build package is then deployed to an AWS S3 bucket.
+Once deployment is successful, an email notification is sent to confirm the deployment.
--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-        <h1>kjhgfdtfyguhijokp'l</h1>
-        <h2>automatic upload and deploy in aws s3 and send a mail notification to successfully deploy</h2>
-    </>
+    <div className="workflow-container">
+      <h2>🚀 Project Deployment Workflow</h2>
+      <ul className="workflow-list no-bullets">
+        <li>Code changes are made and pushed to the Git repository.</li>
+        <li>Jenkins automatically fetches the latest code from Git.</li>
+        <li>Using Jenkins scripts, the project is compiled and a production build is generated.</li>
+        <li>The build package is then deployed to an AWS S3 bucket.</li>
+        <li>Once deployment is successful, an email notification is sent to confirm the deployment.</li>
+      </ul>
+
+    </div>
   )
 }
 
